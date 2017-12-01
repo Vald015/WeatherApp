850b0c0a66e7cc7809ffffe5ee9b3b680ed4e0b1
Osvaldo <osvaldo.ramirez@upr.edu>
2017-11-30 21:38:33 -0400

    implementing google maps error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
+
 const API_KEY = 'cae2c19cf39b68219519c47cc2064d13';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+
+const url = `${ROOT_URL}&q=${city},us`;
+const request = axios.get(url);   //promise
+
+//console.log('Request: ', request );
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };// returning promise as a payload
+}
